bc2b047e062d935da48ba436fd7b8323cc02f93f
TigerWang <tigerwangapps@gmail.com>
2014-10-20 14:26:48 +0800

    Add images for question
    1 Add images for question
    2 Setup Facts View

---
+++ EbolaQuiz/FactsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FactsViewController.h
+//  EbolaQuiz
+//
+//  Created by 王贺 on 14/10/20.
+//  Copyright (c) 2014年 us.hector. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FactsViewController : UIViewController
+
+@end
